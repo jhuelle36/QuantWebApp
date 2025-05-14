0e42b67900b8af0810f942f09d9caed3fd326ca2
jhuelle36 <jhuelle36@gmail.com>
2025-05-14 19:10:42 -0400

    Required commit for AssemblyInfo files on master branch now

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7388019746dff84c3d63f98ebb1e483d9e014596")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc58ff15a0fc05e39d0d6090c3a89ef2d9f828c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
