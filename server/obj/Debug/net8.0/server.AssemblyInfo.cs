55bcd457a9de863c0dea48a908b3e73c4220303d
jhuelle36 <jhuelle36@gmail.com>
2025-05-15 15:08:49 -0400

    Required commit for assembly

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e42b67900b8af0810f942f09d9caed3fd326ca2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a075e6ef561bbf9bbe4a35d3dcdd5795ff7e7eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
