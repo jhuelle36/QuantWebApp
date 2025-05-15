0bbb6abe7e8ee6de43773d74166a159f510b7826
jhuelle36 <jhuelle36@gmail.com>
2025-05-15 15:28:01 -0400

    Added .gitignore file to setup/FinancialModelingPrepAPI branch for ignoring /obj and /bin folder paths

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1c01cd637caaf2452b7c42d5284aea8feb9b32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+124f45180adea3d1c953789324674394dde06344")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
